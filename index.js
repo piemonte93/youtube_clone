4bcfa9b8de3596a4af929c7fd8f055b7e84f48ca
ChaDongHyun <b07882002@gmail.com>
2021-03-06 14:44:51 +0900

    Apply express middleware(morgan, helmet, cookieParser, bodyParser)

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -7,6 +12,12 @@ const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
